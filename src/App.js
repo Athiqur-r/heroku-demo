240f9660386c2ffdf60fe45db01765d12017e56e
Athiqur-r <70250360+Athiqur-r@users.noreply.github.com>
2021-11-11 13:00:02 +0530

    Label Added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello Pradeep !!!</h1>
+        <h1>Welcome to Innomick Technologies !!!</h1>
       </header>
     </div>
   );
